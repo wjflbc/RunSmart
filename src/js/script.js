d24cb12b5dc214fec929b378017ab2cc2aba7e8d
Anastasia <ankalina2796@gmail.com>
2021-05-26 16:25:36 +0300

    Readi js file for work of forms, slow scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -21,9 +21,9 @@ $(document).ready(function(){
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
-    function toggleSlide(item){
-        $('item').each(function (i){
-            $(this).on('click', function (e){
+    function toggleSlide(item) {
+        $('item').each(function(i) {
+            $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -84,6 +84,36 @@ $(document).ready(function(){
     validateForms('#order form');
 
     $('input[name=phone]').mask("(999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+           $(this).find("input").val("");
+           $('#consultation, #order').fadeOut();
+           $('.overlay, #thanks').fadeIn('slow');
+
+           $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    $(window).scroll(function () {
+       if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
 
 
