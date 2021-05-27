ba006689a4405c149f002d97885ab5cb6f90477c
Anastasia <ankalina2796@gmail.com>
2021-05-27 17:43:26 +0300

    Gave src/ in way js 5,6 line

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carusel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow:"<button type=\"button\" class=\"slick-prev\"><img src='icons/chevron-left-solid.png'></button>",
-        nextArrow: "<button type=\"button\" class=\"slick-next\"><img src='icons/chevron-right-solid.png'></button>",
+        prevArrow:"<button type=\"button\" class=\"slick-prev\"><img src='src/icons/chevron-left-solid.png'></button>",
+        nextArrow: "<button type=\"button\" class=\"slick-next\"><img src='src/icons/chevron-right-solid.png'></button>",
         responsive: [
             {
                 breakpoint: 992,
