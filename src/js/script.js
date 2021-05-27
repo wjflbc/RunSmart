ad47c2139ea5addf06033c92e1df5aafdff4395d
Anastasia <ankalina2796@gmail.com>
2021-05-27 13:26:23 +0300

    Took coment  from 115 line

--- src/js/script.js
+++ src/js/script.js
@@ -111,7 +111,7 @@ $(document).ready(function(){
 
     $("a[href^='#']").click(function(){
         const _href = $(this).attr("href");
-        // $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     });
 
