0b0dc79bc3c56a5e577765d0b6be6c67700a8ad7
Anastasia <ankalina2796@gmail.com>
2021-05-28 11:06:03 +0300

    replaced way in the files html and js. Files js works

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carusel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow:"<button type=\"button\" class=\"slick-prev\"><img src='icons/chevron-left-solid.png'></button>",
-        nextArrow: "<button type=\"button\" class=\"slick-next\"><img src='icons/chevron-right-solid.png'></button>",
+        prevArrow:"<button type=\"button\" class=\"slick-prev\"><img src='src/icons/chevron-left-solid.png'></button>",
+        nextArrow: "<button type=\"button\" class=\"slick-next\"><img src='src/icons/chevron-right-solid.png'></button>",
         responsive: [
             {
                 breakpoint: 992,
@@ -89,7 +89,7 @@ $(document).ready(function(){
         e.preventDefault();
         $.ajax({
             type: "POST",
-            url: "mailer/smart.php",
+            url: "src/mailer/smart.php",
             data: $(this).serialize()
         }).done(function() {
            $(this).find("input").val("");
